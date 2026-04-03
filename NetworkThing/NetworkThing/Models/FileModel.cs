5ea8be0b4f988cb03931ee61dbb086f78f9fded3
Ammar <alsoammar1812@gmail.com>
2026-04-03 09:24:57 +0200

    Finished frontend and added API

---
+++ NetworkThing/NetworkThing/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NetworkThing.Models;
+
+public partial class FileModel : ObservableObject
+{
+    [ObservableProperty]
+    private string fileName;
+
+    public FileModel(string name)
+    {
+        FileName = name;
+    }
+}
--- NetworkThing/NetworkThing/ViewModels/MainViewModel.cs
+++ NetworkThing/NetworkThing/ViewModels/MainViewModel.cs
@@ -1,8 +1,18 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using NetworkThing.Models;
 
 namespace NetworkThing.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty] private ObservableCollection<FileModel> fileList = new();
+
+    public MainViewModel()
+    {
+        FileList.Add(new FileModel("file1.txt"));
+        FileList.Add(new FileModel("file2.txt"));
+        FileList.Add(new FileModel("file3.txt"));
+    }
 }
